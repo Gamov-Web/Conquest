3935d814aa94e80847221c9c3884cb339762e963
Vadim <50245404+Zitronius@users.noreply.github.com>
2019-07-26 20:31:43 +0300

    Доделал слайдер и сверстал блок новостей

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
-$(function () {
+$(document).ready(function () {
     $('.slider-item').slick({
+        draggable: false,
         slidesToShow: 1,
-        prevArrow: $('arrow__left'),
-        nextArrow: $('arrow__right'),
-        dotsClass: $('item-number')
+        prevArrow: $('.arrow__left'),
+        nextArrow: $('.arrow__right')
     });
+    $(".slider-item").on('afterChange', function(event, slick, currentSlide){
+        $(".item-number").text(currentSlide + 1);
+   });
 });
