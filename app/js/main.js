df4ea825f7ef8f66a490c8b09f2ce79d2f0439b4
Vadim <50245404+Zitronius@users.noreply.github.com>
2019-08-03 22:40:56 +0300

    Доделал слайдер с ремешками

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,11 @@ $(document).ready(function () {
     $(".slider-item").on('afterChange', function(event, slick, currentSlide){
         $(".item-number").text(currentSlide + 1);
    });
+   $('.slider__items').slick({
+        draggable: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.slider__left'),
+        nextArrow: $('.slider__right')
+   })
 });
